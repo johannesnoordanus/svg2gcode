62f896e78dbe235d0cec9e599fc149c42fc9ae14
johannesnoordanus <52376919+johannesnoordanus@users.noreply.github.com>
2023-08-09 11:04:28 +0200

    CLI change!
    Removed option --maxlaserpower.

--- svg2gcode/__main__.py
+++ svg2gcode/__main__.py
@@ -21,10 +21,9 @@ def svg2gcode(args) -> int:
     try:
         # Instantiate a compiler, specifying the interface type and the speed at which the tool should move (for both image drawings and laser cutting).
         # For line drawings 'pass_depth' controls how far down the tool moves after every pass. Set it to 0 if your machine does not support Z axis movement.
-        gcode_compiler = Compiler(interfaces.Gcode, params={'laser_power':args.cuttingpower,'movement_speed':args.cuttingspeed,
-                            'maximum_laser_power':args.maxlaserpower,'pixel_size':args.pixelsize,'maximum_image_laser_power':args.imagepower,
-                            'image_movement_speed':args.imagespeed,'fan':args.fan,'rapid_move':args.rapidmove,'showimage':args.showimage,
-                            'x_axis_maximum_travel':args.xmaxtravel,'y_axis_maximum_travel':args.ymaxtravel})
+        gcode_compiler = Compiler(interfaces.Gcode, params={'laser_power':args.cuttingpower,'movement_speed':args.cuttingspeed, 'pixel_size':args.pixelsize,
+                        'maximum_image_laser_power':args.imagepower, 'image_movement_speed':args.imagespeed, 'fan':args.fan,'rapid_move':args.rapidmove,
+                        'showimage':args.showimage, 'x_axis_maximum_travel':args.xmaxtravel,'y_axis_maximum_travel':args.ymaxtravel})
         # emit gcode for svg
         gcode_compiler.compile_to_file(args.gcode, parse_file(args.svg), passes=1)
 
@@ -43,8 +42,8 @@ def main() -> int:
     imagespeed_default = 800
     cuttingspeed_default = 1000
     imagepower_default = 300
-    cuttingpower_default = .85
-    maxlaserpower_default = 1000
+    cuttingpower_default = 850
+    laserpower_default = 1000
     xmaxtravel_default = 300
     ymaxtravel_default = 400
 
@@ -62,9 +61,7 @@ def main() -> int:
     parser.add_argument('--imagepower', default=imagepower_default, metavar="<default:" +str(imagepower_default)+ ">",
         type=int, help="maximum laser power while drawing an image (as a rule of thumb set to 1/3 of the machine maximum)")
     parser.add_argument('--cuttingpower', default=cuttingpower_default, metavar="<default:" +str(cuttingpower_default)+ ">",
-        type=float, help="percentage of maximum laser power for line drawings/cutting")
-    parser.add_argument('--maxlaserpower', default=maxlaserpower_default, metavar="<default:" +str(maxlaserpower_default)+ ">",
-        type=int, help="maximum laser power of laser cutter")
+        type=int, help="sets laser power of line drawings/cutting")
     parser.add_argument('--rapidmove', action='store_true', default=True, help='generate inbetween G0 moves' )
     parser.add_argument('--xmaxtravel', default=xmaxtravel_default, metavar="<default:" +str(xmaxtravel_default)+ ">",
         type=int, help="machine x-axis lengh in mm")
